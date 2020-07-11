a153167f3a75633875ab6d3bf6c6f16dc5bfd73d
Dudceac Octavian <48602187+Tavy7@users.noreply.github.com>
2020-07-11 14:45:39 +0300

    Add files via upload

---
+++ src/createBot.py
@@ -0,0 +1,86 @@
+import pyautogui as py
+from zona import Zona
+import keyboard
+import time
+
+def afisareMeniu():
+	print(' 1 - Adauga punct pentru click')
+	print(' 2 - Click dreapta')
+	print(' 3 - Adauga text (prima interogare va fi unde sa dea click pt a scrie')
+	print(' Eventuri:')
+	print(' 4 - Adauga zona de verificat hover')
+	print(' 5 - Adauga pauza pana la keypress')
+
+	print('\nApasa q pentru iesire.')
+
+def adaugaZona():
+	coordonate = []
+	print('Selecteaza coltul din NW si cel din SE.')
+	print('Apasa f cand mouse-ul e in pozitie pentru a memora coordonata.')
+
+	#memoreaza coltul NW, ce contine (xmin, ymin) si coltul SE (xmax, ymax)
+	while len(coordonate) < 2:
+		pos = py.position()
+
+		if keyboard.is_pressed('f'):
+			time.sleep(0.5)# sleep pentru a nu da append de mai multe ori
+			coordonate.append(pos)
+			continue
+
+	print('\n\nZona memorata.')
+	return Zona(coordonate)
+
+def adaugaPunctClick():
+	print('Apasa f cand mouse-ul e in pozitie pentru a memora coordonata.')
+
+	while True:
+		pos = py.position()
+		print(pos)
+
+		if keyboard.is_pressed('f'):
+			print('\n\nZona memorata')
+			return pos
+
+def adaugaKeypress():
+	return input('Key: ')
+
+def adaugaText():
+	return input('Text de adaugat: ')
+
+def main():
+	numeBot = input('Cum se numeste bot-ul?\n')
+	file = open('boti\\' + numeBot + '.csv', 'a+')
+
+	while True:
+		afisareMeniu()
+		
+		x = input('Alege actiune: ')
+		if x not in ['1', '2', '3', '4', '5', 'q']:
+			print('\nInput incorect!\n')
+			continue
+
+		if x == '1' or x == '3':# x == '3' pentru a stii unde sa introduc text
+			pos = adaugaPunctClick()
+			file.write('c,{},{}\n'.format(pos[0], pos[1]))
+		
+		if x == '2':
+			pos = adaugaPunctClick()
+			file.write('d,{},{}\n'.format(pos[0], pos[1]))
+
+		if x == '3':
+			file.write('s,{}\n'.format(adaugaText()))
+
+		if x == '4':
+			zo = adaugaZona()
+			file.write('z,{},{},{},{}\n'.format(zo.xmin, zo.ymin, zo.xmax, zo.ymax))
+
+		if x == '5':
+			file.write('k,{}\n'.format(adaugaKeypress()))
+
+		if x == 'q':
+			break
+
+	file.close()
+	print("Bot creeat!")
+
+main()
---
+++ src/runBot.py
@@ -0,0 +1,83 @@
+import pyautogui as py
+from zona import Zona
+import keyboard
+
+py.FAILSAFE = True
+py.PAUSE = 0.1
+
+# functie de pauza pana la apasare de buton
+def pauza(key):
+	print('Asteptam keypress ' + key)
+	while True:
+		if keyboard.is_pressed(key):
+			break
+
+def hoverZone(coordonate):
+	coordonate = list(map(int, coordonate))# toate elementele sunt int
+	# xmin ymin, xmax, ymax 
+	zo = Zona([(coordonate[0], coordonate[1]), (coordonate[2], coordonate[3])])
+
+	while True:
+		print('Hover zone \n' + str(zo))
+		x, y = py.position()
+		
+		cond = zo.onButton(x, y)
+		if cond == True:
+			return
+
+def getBot():
+	import os
+	bots = os.listdir('boti/')
+
+	if len(bots) == 0:
+		print('Nu exista boti')
+		quit()
+	
+	print('Alege bot')
+	cnt = 1
+	for bot in bots:
+		print(' {} - {}'.format(cnt, bot))
+		cnt += 1
+
+	choice = input("\n")
+	
+	try:
+		choice = int(choice) - 1
+		if choice < cnt - 1:
+			return bots[choice]
+		return getBot()
+	except:
+		print('Valoarea trebuie sa fie int')
+
+
+def getActions(botName):
+	file = open('boti/' + botName, 'r')
+	actions = file.readlines()
+
+	for action in actions:
+		action = action.split(',')
+	
+		action[-1] = str(action[-1][:-1]) # stergem \n de la final
+
+		if action[0] == 'k':# wait pana la keypress
+			pauza(action[1])
+
+		if action[0] == 'z':# wait pana la hover pe o anumita zona
+			hoverZone(action[1:])
+
+		if action[0] == 'c': # click
+			print('Efectuam click la {}, {}'.format(action[1], action[2]))
+			py.click(int(action[1]), int(action[2]))
+		
+		if action[0] == 'd': # click dr
+			print('Efectuam click dreapta la {}, {}'.format(action[1], action[2]))
+			py.click(int(action[1]), int(action[2]), button = 'right')
+			
+		if action[0] == 's': #text
+			py.write(action[1])
+			
+py.alert('Bot running')
+botName = getBot()
+
+while py.FAILSAFE:
+	getActions(botName)
